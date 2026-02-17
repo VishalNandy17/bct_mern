032d8a72b60dd13c95fe57c028300bf99e50bd94
VishalNandy17 <nandyvishal17@gmail.com>
2026-02-17 12:50:22 +0530

    more components here

--- BCT/src/App.jsx
+++ BCT/src/App.jsx
@@ -1,11 +1,17 @@
 import Home from './component/Home'
+import About from './component/About'
+import Contact from './component/contact'
+import Services from './component/services'
 
 function Vishal() {
-  let name = "Nabanita"
+  let name = "Vishal"
   return (
     <div>
       <h1>Hello React from {name}</h1>
       <Home />
+      <About />
+      <Contact />
+      <Services />
     </div>
   )
 }
